88103b2e559644a8cb0078a55a70c66334fc033b
Jun <wnsgur4092@gmail.com>
2023-07-29 15:43:05 +1000

    Initial Commit

---
+++ Boomerang/BoomerangApp.swift
@@ -0,0 +1,20 @@
+//
+//  BoomerangApp.swift
+//  Boomerang
+//
+//  Created by JunHyuk Lim on 29/7/2023.
+//
+
+import SwiftUI
+
+@main
+struct BoomerangApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
