6840237f60333bd3102de6fa2d67252b12bdbe0a
Jun <wnsgur4092@gmail.com>
2023-10-20 03:30:30 +1100

    Refactor OnBoardingView

---
+++ Boomerang/View/OnBoarding/OnBoardingPhase.swift
@@ -0,0 +1,18 @@
+//
+//  OnBoardingPhase.swift
+//  Boomerang
+//
+//  Created by JunHyuk Lim on 20/10/2023.
+//
+
+import SwiftUI
+
+struct OnBoardingPhase: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OnBoardingPhase()
+}
