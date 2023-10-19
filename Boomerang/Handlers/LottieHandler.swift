2ec7028118e6016654863239dd2b83c30c5de853
Jun <wnsgur4092@gmail.com>
2023-10-20 03:20:01 +1100

    Add feature for OnBoardingView

---
+++ Boomerang/Handlers/LottieHandler.swift
@@ -0,0 +1,8 @@
+//
+//  LottieHandler.swift
+//  Boomerang
+//
+//  Created by JunHyuk Lim on 20/10/2023.
+//
+
+import Foundation
---
+++ Boomerang/View/OnBoarding/OnBoardingView.swift
@@ -0,0 +1,18 @@
+//
+//  OnBoardingView.swift
+//  Boomerang
+//
+//  Created by JunHyuk Lim on 20/10/2023.
+//
+
+import SwiftUI
+
+struct OnBoardingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OnBoardingView()
+}
